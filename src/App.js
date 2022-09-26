d7c9496a416929c26d9285292bcc23cc4c9bac2b
mirzavic4869 <avicenamirzam@gmail.com>
2022-09-26 21:29:05 +0700

    fetching advice API

--- src/App.js
+++ src/App.js
@@ -7,12 +7,27 @@ export default class App extends Component {
 	state = { quote: "" };
 
 	componentDidMount() {
-		console.log("COMPONENT DID MOUNT");
+		this.fetchQuote();
 	}
+
+	fetchQuote = () => {
+		axios
+			.get("https://api.adviceslip.com/advice")
+			.then((response) => {
+				const { advice } = response.data.slip;
+				this.setState({ quote: advice });
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 	render() {
+		const { quote } = this.state;
 		return (
-			<div>
-				<h1>App</h1>
+			<div className="app">
+				<div className="card">
+					<h1 className="heading">{quote}</h1>
+				</div>
 			</div>
 		);
 	}
